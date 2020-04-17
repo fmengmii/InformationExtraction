a1512ac83877d423f5e6174b7911bbbbff3c83db
frankmeng <frankmeng@172.20.10.7>
2020-04-17 13:37:59 -0400

    adding new utility to clean docs

---
+++ src/util/CleanDocs.java
@@ -0,0 +1,67 @@
+package util;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import utils.db.DBConnection;
+
+public class CleanDocs
+{
+
+	public CleanDocs()
+	{
+	}
+	
+	public void clean(String user, String password, String config)
+	{
+		try {
+			Properties props = new Properties();
+			props.load(new FileReader(config));
+			String host = props.getProperty("host");
+			String dbName = props.getProperty("dbName");
+			String dbType = props.getProperty("dbType");
+			String idCol = props.getProperty("idCol");
+			String textCol = props.getProperty("textCol");
+			String docTable = props.getProperty("docTable");
+			
+			Connection conn = DBConnection.dbConnection(user, password, host, dbName, dbType);
+			Connection conn2 = DBConnection.dbConnection(user, password, host, dbName, dbType);
+			Statement stmt = conn.createStatement();
+			PreparedStatement pstmt = conn2.prepareStatement("update " + docTable + " set " + textCol + " = ? where document_id = ?");
+			
+			conn2.setAutoCommit(false);
+			
+			int count = 0;
+			
+			ResultSet rs = stmt.executeQuery("select " + idCol + ", " + textCol + " from " + docTable);
+			while (rs.next()) {
+				long docID = rs.getLong(1);
+				String text = rs.getString(2);
+				text = text.trim();
+				
+				pstmt.setString(1, text);
+				pstmt.setLong(2, docID);
+				pstmt.addBatch();
+				count++;
+				
+				if (count == 100) {
+					pstmt.executeBatch();
+					conn2.commit();
+					count = 0;
+				}
+			}
+			
+			pstmt.executeBatch();
+			conn2.commit();
+			
+			conn.close();
+			conn2.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
