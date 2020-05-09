1d62ba88c911cea7264167b8df13ddbe3159c16c
frankmeng <frankmeng@172.20.10.7>
2020-05-08 22:44:01 -0400

    total not used in readfinal

--- src/msa/ProfileReader.java
+++ src/msa/ProfileReader.java
@@ -205,7 +205,7 @@ public class ProfileReader
 		ResultSet rs = stmt.executeQuery("select a.profile_id, a.target_id, b.profile, b." + rq + "group" + rq + ", c.profile "
 			+ "from " + finalProfileTable + " a, " + profileTable + " b, " + profileTable + " c "
 			+ "where b.annotation_type = '" + annotType + "' and b.score >= 0.0 and b.profile_type = " + profileType + " and a.profile_id = b.profile_id and a.target_id = c.profile_id and "
-			+ "a.disabled = 0 and a.prec >= " + prec);
+			+ "a.disabled = 0 and a.prec >= " + prec + " and a.total >= " + total);
 		
 		Map<Long, MSAProfile> profileMap2 = new HashMap<Long, MSAProfile>();
 		
