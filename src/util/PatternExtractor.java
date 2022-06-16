e351e0c3a88fee6357ee1077ef5fbccbfb4bede2
frankmeng <frankmeng@172.20.10.5>
2022-06-16 09:46:01 -0400

    import of pattern extractor

---
+++ src/util/PatternExtractor.java
@@ -0,0 +1,123 @@
+package util;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+import utils.db.DBConnection;
+
+public class PatternExtractor 
+{
+	private Connection conn;
+	private String annotTable;
+	private String schema;
+	private PreparedStatement pstmt;
+
+	public PatternExtractor()
+	{
+	}
+	
+	public void extract(String user, String password, String config)
+	{
+		try {
+			Properties props = new Properties();
+			props.load(new FileReader(config));
+			
+			String host = props.getProperty("host");
+			String dbName = props.getProperty("dbName");
+			String dbType = props.getProperty("dbType");
+			schema = props.getProperty("schema");
+			String indexTable = props.getProperty("indexTable");
+			String finalTable = props.getProperty("finalTable");
+			String annotType = props.getProperty("annotType");
+			annotTable = props.getProperty("annotTable");
+			
+			conn = DBConnection.dbConnection(user, password, host, dbName, dbType);
+			String rq = DBConnection.reservedQuote;
+			
+			pstmt = conn.prepareStatement("select value from " + schema + annotTable + " where document_id = ? "
+				+ "and start >= ? and " + rq + "end" + rq + " <= ? and annotation_type = 'Token' "
+				+ "order by start");
+
+			Statement stmt = conn.createStatement();
+			
+			Map<String, Integer> countMap = new HashMap<String, Integer>();
+			
+			ResultSet rs = stmt.executeQuery("select a.profile_id, a.document_id, a.start, a." + rq + "end" + rq
+				+ " from " + schema + indexTable + " a "
+				+ "where a.profile_id in "
+				+ "(select b.profile_id from " + schema + finalTable + " b "
+				+ "where b.annotation_type = '" + annotType + "' and disabled = 0)");
+			
+			while (rs.next()) {
+				int profileID = rs.getInt(1);
+				long docID = rs.getLong(2);
+				long start = rs.getLong(3);
+				long end = rs.getLong(4);
+				
+				String pattern = getPatternStr(docID, start, end);
+				Integer count = countMap.get(pattern);
+				
+				if (count == null) {
+					count = 0;
+				}
+				
+				countMap.put(pattern, ++count);
+			}
+			
+			List<String> pattList = new ArrayList<String>();
+			List<Integer> countList = new ArrayList<Integer>();
+			
+			for (String patt : countMap.keySet()) {
+				int count = countMap.get(patt);
+				
+				boolean inserted = false;
+				for (int i=0; i<countList.size(); i++) {
+					if (count > countList.get(i)) {
+						countList.add(i, count);
+						pattList.add(i, patt);
+						inserted = true;
+						break;
+					}
+				}
+				
+				if (!inserted) {
+					countList.add(count);
+					pattList.add(patt);
+				}
+			}
+			
+			for (int i=0; i<pattList.size(); i++) {
+				System.out.println(pattList.get(i) + "\ncount: " + countList.get(i) + "\n\n");
+			}
+			
+			
+			conn.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private String getPatternStr(long docID, long start, long end) throws SQLException
+	{
+		StringBuilder strBlder = new StringBuilder();
+		Statement stmt = conn.createStatement();
+		
+		pstmt.setLong(1, docID);
+		pstmt.setLong(2, start);
+		pstmt.setLong(3, end);
+		ResultSet rs = pstmt.executeQuery();
+		
+		while (rs.next()) {
+			String val = rs.getString(1);
+			
+			strBlder.append(val + " ");
+		}
+		
+		stmt.close();
+		
+		return strBlder.toString().trim();
+	}
+}
